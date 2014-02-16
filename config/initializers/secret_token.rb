c5e01de6dc1eedf68a8fe280b3179d3388850622
bkdir <direkburak1@gmail.com>
2014-02-16 01:35:19 -0500

    initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imdp::Application.config.secret_key_base = '1edf3994b12f02a2c699ef060bcaebe459a7de738b2f5ea1880b1a4cd75f356595fb536062667bfaf7470670b941ec0a22ad8bb73225fc68a6a2a4ddcf676ef0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imdp::Application.config.session_store :cookie_store, key: '_imdp_session'
