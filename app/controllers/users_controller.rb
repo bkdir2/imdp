46a039da4e5373729a87cd79db77d654513faab4
bkdir <direkburak1@gmail.com>
2014-02-17 17:21:08 -0500

    new-edit partiallara ayrildi, buton ismi ayarlandi, update func. yazildi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,49 @@
 class UsersController < ApplicationController
   
   def index
-	@users = User.all
+		@users = User.all
   end
 
+
   def new
-	@user = User.new
+		@user = User.new
   end
+
 
   def create
   	@user = User.new(accessible_params)
-	if @user.save
-		flash[:info] = "Successfully Registered"
-		redirect_to @user
-	else
-		flash[:error] = "Could not Registered"
-		render "new"
-	end
+		if @user.save
+			flash[:info] = "Successfully Registered"
+			redirect_to @user
+		else
+			flash[:error] = "Could not Registered"
+			render "new"
+		end
   end
 
-  def show
-	@user = User.find(params[:id])
+
+  def edit
+  	@user = User.find(params[:id])
   end
+
+
+	#dogrulugundan emin degilim update method.
+  def update
+  	@user = User.find(params[:id])
+		if @user.update_attributes(accessible_params)
+			flash[:info] = "Updated"
+			redirect_to @user
+		else
+			flash[:error] = "Could not Updated"
+			render "edit"
+		end
+  end
+
+
+  def show
+		@user = User.find(params[:id])
+  end
+  
 end
 
 
